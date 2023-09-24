4cf209e052afa22a69f190452bb09bb4db9c0c85
joymohammod83 <mdjoysarkar83@gmail.com>
2023-09-24 21:59:34 +0600

    multify

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function multiply(num){
+    console.log(`Multiplication for ${num}`)
+    for(let i=1; i<=10; i++){
+         item  = i * num; 
+        console.log( `${i} * ${num} = `, item ) 
+    }
+}
+
+multiply(2)
+multiply(3)
+multiply(4)
+multiply(5)
+multiply(6)
+multiply(7)
+multiply(8)
+multiply(9)
+multiply(10)
+multiply(11)
+multiply(12)
+multiply(13)
+multiply(14)
+multiply(15)
+multiply(16)
+multiply(17)
+multiply(18)
+multiply(19)
+multiply(20)
+multiply(21)
+multiply(22)
+multiply(23)
+multiply(24)
+multiply(25)
